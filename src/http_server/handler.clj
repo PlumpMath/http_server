81a21c6544eda97b4d25caa334f958b8af670770
Robert Johansson <robert@eailab.com>
2015-06-15 15:37:07 -0500

    Allow more arbitrary files than file1 and file2

--- src/http_server/handler.clj
+++ src/http_server/handler.clj
@@ -1,5 +1,6 @@
 (ns http-server.handler
   (:require [clojure.string :as str]
+            [clojure.java.io :as io]
             [http-server.files :as files]
             [http-server.operations :as op]))
 
@@ -19,6 +20,13 @@
         b64-string (if (auth-in-header? headers)
                      (-> headers first (str/split #" ") last))]
     (if b64-string (= userid-password (files/base64-decode b64-string)))))
+
+(defn file-in-directory? [uri directory]
+  (let [PUB_DIR directory
+        file (subs uri 1)
+        files (rest (file-seq (io/file PUB_DIR)))
+        filenames (map #(.getName %) files)]
+    (some #{file} filenames)))
 
 (defn get-handler [{:keys [uri extension query headers] :as request} directory]
   (cond query
@@ -70,7 +78,7 @@
             (assoc :status (op/status 200)
                    :header (op/header :text)
                    :body (op/show-file uri directory)))
-        (some #(= uri %) ["/file1" "/file2"])
+        (file-in-directory? uri directory)
         (-> (empty-response)
             (assoc :status (op/status 200)
                    :header (op/header :text) ;; Assumed to be text if no ext
