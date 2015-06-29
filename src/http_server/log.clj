9af142724e266316833c9c34f1409da0df9be674
Robert Johansson <robert@eailab.com>
2015-06-29 11:53:26 -0500

    v0.1 finished fix

--- src/http_server/log.clj
+++ src/http_server/log.clj
@@ -1,4 +1,6 @@
 (ns http-server.log)
 
 (defn log [s]
-  (spit "tmp/http_server.log" (str s "\n\n") :append true))
+  (let [TMP_DIR (System/getProperty "TMP_DIR")
+        file (clojure.java.io/file (str TMP_DIR "/http_server.log"))]
+    (spit file (str s "\n\n") :append true)))
